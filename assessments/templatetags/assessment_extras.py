b7496dd74b501671bd092ea049a64fc43f15407d
Hossein <hossein@example.com>
2025-12-23 16:00:08 +0330

    mini update

--- assessments/templatetags/assessment_extras.py
+++ assessments/templatetags/assessment_extras.py
@@ -1,29 +1,10 @@
 """
-Template tags for core app
+Template tags for assessments app
 """
 import re
 from django import template
-from core.utils import get_country_name, get_device_type_display
 
 register = template.Library()
-
-
-@register.filter(name='get_country_name')
-def get_country_name_filter(country_code):
-    """
-    تبدیل کد کشور به نام کشور (فارسی)
-    """
-    if not country_code:
-        return 'نامشخص'
-    return get_country_name(country_code)
-
-
-@register.filter(name='get_device_type_display')
-def get_device_type_display_filter(device_type):
-    """
-    تبدیل نوع دستگاه به نام فارسی
-    """
-    return get_device_type_display(device_type)
 
 
 @register.filter(name='extract_youtube_id')
